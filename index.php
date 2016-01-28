b7cc87aa2c9b89a3e35edb6c6ab4a48bc2a1b2f9
gflor <gflor@Zyalalys-PC>
2016-01-28 17:41:31 +0100

    Riprogettazione sito

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
+<!-- Connessione al database -->
 <?php
-    include("connect_database.php");
+    //include("connect_database.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +9,18 @@
         <title></title>
     </head>
     <body>
-        <div></div>
+        <div id="header">
+            <div class="container_12">
+                <h1 class="grid_3" id="logo"><a href="index.php">BubiShop</a></h1>
+
+                <ul class="grid_5 prefix_4" id="menu">
+                    <li><a class="active" href="index.php">Home</a></li>
+                    <li><a href="showcategories.php">Prodotti</a></li>
+                    <li><a href="carrello.php">Carrello</a></li>
+                    <li><a href="info.php">Su Di Noi..</a></li>
+                </ul>
+            </div>
+        </div>
         <?php
         // put your code here
         ?>
