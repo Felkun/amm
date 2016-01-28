14a28d914d3f076e9a388ac110e79d9d05a45d46
gflor <gflor@Zyalalys-PC>
2016-01-28 20:10:01 +0100

    Riprogettazione sito

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- Connessione al database -->
 <?php
-    //include("connect_database.php");
+    //include("connect.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,16 +10,7 @@
     </head>
     <body>
         <div id="header">
-            <div class="container_12">
-                <h1 class="grid_3" id="logo"><a href="index.php">BubiShop</a></h1>
-
-                <ul class="grid_5 prefix_4" id="menu">
-                    <li><a class="active" href="index.php">Home</a></li>
-                    <li><a href="showcategories.php">Prodotti</a></li>
-                    <li><a href="carrello.php">Carrello</a></li>
-                    <li><a href="info.php">Su Di Noi..</a></li>
-                </ul>
-            </div>
+            <h1 class="grid_3" id="logo"><a href="index.php">Metroid Prime</a></h1>
         </div>
         <?php
         // put your code here
