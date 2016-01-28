26e0aa9f7ee15f3c535e19a117f0d5814e82b1b9
gflor <gflor@Zyalalys-PC>
2016-01-28 22:53:22 +0100

    Pova bottoni

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
             <h1 class="logo"><a href="index.php">Metroid Prime Website</a></h1>
         
         <ul id="menu">
-            <li><a class="active" href="index.php">Home</a></li>
-            <li><a href="series.php">La Serie</a></li>
-            <li><a href="story.php">La Storia</a></li>
-            <li><a href="gameplay.php">Gameplay</a></li>
-            <li><a href="shop.php">Shop</a></li>
+            <li><a class="active" href="index.php"><span class="menu">Home</span></a></li>
+            <li><a href="series.php"><span class="menu">La Serie</span></a></li>
+            <li><a href="story.php"><span class="menu">La Storia</span></a></li>
+            <li><a href="gameplay.php"><span class="menu">Gameplay</span></a></li>
+            <li><a href="shop.php"><span class="menu">Shop</span></a></li>
         </ul>
         </div>
         
