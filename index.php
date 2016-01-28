7fc8617ff07b288da2dc549844ec4560b40466ee
Zyalalys <gfloris91@gmail.com>
2016-01-28 21:03:38 +0100

    Riprogettazione sito

--- index.php
+++ index.php
@@ -6,12 +6,21 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
         <title></title>
     </head>
     <body>
         <div id="header">
-            <h1 class="grid_3" id="logo"><a href="index.php">Metroid Prime</a></h1>
+            <h1 class="logo"><a href="index.php">Metroid Prime Website</a></h1>
+        
+        <ul id="menu">
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="showcategories.php">La Serie</a></li>
+            <li><a href="carrello.php">La Storia</a></li>
+            <li><a href="info.php">Shop</a></li>
+        </ul>
         </div>
+        
         <?php
         // put your code here
         ?>
