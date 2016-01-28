01854e15406e21c80b089816a81a8918bf427445
amm <amm@amm-VirtualBox>
2016-01-28 17:31:55 +0100

    Riprogettazione sito

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include("connect_database.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div></div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
