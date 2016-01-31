97652f71c132d916c6213862a633fccdadf7df05
gflor <gflor@Zyalalys-PC>
2016-01-31 21:24:42 +0100

    Sviluppo grafica

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         <div id="container">
             <div id="header">
                 <h1 class="logo"><a href="index.php">Metroid Prime Website</a></h1>
-            
                 <ul id="menu">
                     <li><a class="active" href="index.php"><span class="menu">Home</span></a></li>
                     <li><a href="series.php"><span class="menu">La Serie</span></a></li>
@@ -24,19 +23,17 @@
                     <li><a href="gameplay.php"><span class="menu">Gameplay</span></a></li>
                     <li><a href="shop.php"><span class="menu">Shop</span></a></li>
                 </ul>
-                
             </div>
-            
             <div id="body">
-                <div><center><video width="560" height="315" <!--autoplay="autoplay"--> preload="(none,auto,metadata)" poster="Tua immagine.jpg" controls="controls" loop="loop">
+                
+                <div><center><video width="560" height="315" autoplay="none" preload="(none,auto,metadata)" poster="Tua immagine.jpg" controls="controls" loop="loop">
                     <source src="media/mpt_trailer.webm" type="video/mp4" />
                 </video></center></div>
             </div>
             
             <div id="push"></div>
-        
-        </div>
-        
+            
+        </div>  
     <div id="footer">
         <center><div>Copyright &#169; 2002-2016 Nintendo</div></center>
     </div>
